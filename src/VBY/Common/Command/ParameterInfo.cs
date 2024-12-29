0ecc0458bcb8e4432a1dadc04f3e90f19da3d18c
Cai <13110818005@qq.com>
2024-12-29 15:43:49 +0800

    handle null warning

--- src/VBY/Common/Command/ParameterInfo.cs
+++ src/VBY/Common/Command/ParameterInfo.cs
@@ -2,6 +2,6 @@
 
 public class ParameterInfo
 {
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
 }

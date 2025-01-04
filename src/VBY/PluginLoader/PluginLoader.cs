ab37b71a19cbfa883c47d78df834219ae29f41a4
Cai <13110818005@qq.com>
2025-01-04 14:43:26 +0800

    Update PluginLoader.cs

--- src/VBY/PluginLoader/PluginLoader.cs
+++ src/VBY/PluginLoader/PluginLoader.cs
@@ -20,6 +20,7 @@ namespace VBY.PluginLoader;
 public class PluginLoader : TerrariaPlugin
 {
     public override string Author => "yu";
+    public override string Name => "VBY.PluginLoader";
 
 #pragma warning disable IDE0044 // 添加只读修饰符
 #pragma warning disable CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
